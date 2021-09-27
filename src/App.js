d932544267d4ac0abdc155f32d8f24d891f2aef7
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-09-27 18:05:08 +0530

    tryint to add the layout of the page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-  const [name,setName] = useState('');
-  const [list,setList] = useState([]);
-  const [isEditing,setIsEditing] = useState(false);
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
   const [editId, setEditId] = useState(null);
-  const [alert,setAlert] = useState({show:false,msg:'',type:''});
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
-  return(<section className="section-center">
-    <div className="grocery-container">
-      <List />
-      <button className="clear-btn">clear items</button>
-    </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!name){
+      //handle alert
+    }
+    else if(name && isEditing){
+      //handle different type of alert and some changes
+    }
+    else{
+      //show alert and add new item
+    }
+  }
 
-  </section>);
+  return (
+    <section className="section-center">
+      <form class="grocery-form" onSubmit= {handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+      <div className="form-control">
+        <input
+          type="text"
+          className="grocery"
+          placeholder="e.g., Eggs "
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit" className="submit-btn">
+          {isEditing ? "edit" : "submit"}{" "}
+        </button>
+      </div>
+      </form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">clear items</button>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
